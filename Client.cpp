ac8a9b8d1f9ee470b9ccd15ba6daf68376407a38
mzaeemnasir <m.zaeem.nasir@gmail.com>
2021-12-24 22:26:28 +0500

    Basic Code

--- Client.cpp
+++ Client.cpp
@@ -1,8 +1,40 @@
+
+// Client.cpp
+// ---------------------
+// Creating Socket
+// Sending Data
+// Accept Data
+// Closing Socket
+// ---------------------
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 using namespace std;
 
 int main()
 {
+    int Socket = socket(AF_INET, SOCK_DGRAM, 0);
+    char Buffer[1024] = {0};
+    if (Socket < 0)
+    {
+        cout << "Error in creating socket" << endl;
+        return -1;
+    }
+    else
+    {
+        cout << "Socket Created" << endl;
+    }
+
+    struct sockaddr_in ServerAddress;
+    ServerAddress.sin_family = AF_INET;
+    ServerAddress.sin_port = htons(3000);
+    ServerAddress.sin_addr.s_addr = INADDR_ANY;
+
+    sendto(Socket, "Hello Server", sizeof("Hello Server"), 0, (sockaddr *)&ServerAddress, sizeof(ServerAddress));
+    socklen_t sizeofServerAddress;
+    recvfrom(Socket, Buffer, sizeof(Buffer), 0, (sockaddr *)&ServerAddress, &sizeofServerAddress);
+    cout << "Connection Established " << Buffer << endl;
     return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -1,8 +1,59 @@
+// UDP Socket Programming
+// M Zaeem Nasir
+// 19F 0355
+// 5B
+
+// Server.cpp
+// ---------------------
+// Creating Socket
+// Binding Socket
+// Listening for Client
+// Accepting Client
+// Sending Data
+// Closing Socket
+// ---------------------
+
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 using namespace std;
 
 int main()
 {
+    int Socket = socket(AF_INET, SOCK_DGRAM, 0);
+    char Buffer[1024] = {0};
+    if (Socket < 0)
+    {
+        cout << "Error in creating socket" << endl;
+        return -1;
+    }
+    else
+    {
+        cout << "Socket Created" << endl;
+    }
+
+    struct sockaddr_in ServerAddress, ClientAddress;
+    ServerAddress.sin_family = AF_INET;
+    ServerAddress.sin_port = htons(3000);
+    ServerAddress.sin_addr.s_addr = INADDR_ANY;
+
+    int Bind = bind(Socket, (sockaddr *)&ServerAddress, sizeof(ServerAddress));
+    if (Bind < 0)
+    {
+        cout << "Error in binding socket" << endl;
+        return -1;
+    }
+    else
+    {
+        cout << "Socket Bound" << endl;
+    }
+    memset(&ClientAddress, 0, sizeof(ClientAddress));
+    socklen_t sizeofClientAddress = sizeof(ClientAddress);
+    recvfrom(Socket, Buffer, sizeof(Buffer), 0, (sockaddr *)&ClientAddress, &sizeofClientAddress);
+    cout << "Client: Connected" << Buffer << endl;
+    sendto(Socket, "Hello Client", sizeof("Hello Client"), 0, (sockaddr *)&ClientAddress, sizeof(ClientAddress));
+
     return 0;
 }
